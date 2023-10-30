b0c0d056becebc03b1cac3e3360c41ec7ed9dcf3
quokkawoo <quokkawoo@gmail.com>
2023-10-30 14:20:59 +0900

    헤더 이식 완료

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -1,85 +1,103 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import './../App.css';
-import arrow from './../pic/arrow.jpg'
 
+export default function Header(props){
 
+    const [scrollPosition, setScrollPosition] = useState(null);
+    const [clickHamburger, setClickHamburger] = useState(null);
+    const [clickMenu, setClickMenu] = useState(null);
 
-export default function Header(){
+    // 스크롤 위치 인식
+    const updateScroll = () => {
+        setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    }
 
-    let [list, setList] = useState(['', '', '']);
-    let [counter, setCounter] = useState(1);
-
-
-    useEffect(() => {
-        let timer2 = setTimeout(()=>{document.querySelector('.button-hamberger').addEventListener('click', showList)}, 100);
+    // 무한 스크롤 방지하기 위해서 100ms마다 스크롤 위치 체크
+    useEffect(()=>{
+        const timer = setTimeout(window.addEventListener('scroll', updateScroll), 100)       
         return () => {
-            // 
-            clearTimeout(timer2);
+            clearTimeout(timer);
         };
-        }, [])
+    });
 
-
+    // 햄버거 버튼 클릭시 메뉴 보이기
     const showList = () => {
-        if (counter % 2 == 1) {
-            counter ++;
-            let copy = [...list];
-            copy[0] = 'bg-black';
-            copy[1] = 'button-xline';
-            copy[2] = 'bg-black max-h100vh';
-            setList(copy);
-        } else {
-            counter ++;
-            let copy = [...list];
-            copy[0] = '';
-            copy[1] = '';
-            copy[2] = '';
-            setList(copy);
-        }       
+        setClickHamburger(!clickHamburger);
     };
 
-    const goContent2 = (e) => {
-        // a 태그는 고유 이벤트가 링크 주소로 이동하는 것이기에 js스크립트 안의 내용을 실행하기 전에 고유 이벤트 실행을 하고 끝냄. 따라서 이를 막아주는 e.preventDefault()를 적어줘야 아래의 코드를 실행함.
-        e.preventDefault();
-        window.scrollTo(0, 1000);
+    // 모바일 내 메뉴 클릭시 서브 메뉴 보이기
+    const showSubMenu = (e) => {
+        setClickMenu(e);
     }
-    const goContent3 = (e) => {
-        e.preventDefault();
-        window.scrollTo(0, 3000);
-    }
-    const goContent4 = (e) => {
-        e.preventDefault();
-        window.scrollTo(0, 3500);
-    }
+
+
 
     return (
         <>
-            <nav className={'p-navbar p-navbar--dark ' + list[0]}>
-                <div className='p-container p-container--default'>
-                    <div className='p-container__inner p-navbar__inner-container'>
-                        <div className='p-navbar__logo-container'>
-                            <a className='c-white p-navbar__logo-icon' href='#'>DAMLASH</a>
-                        </div>
-                        <div className='p-navbar__mobile'>
-                            <button className={'button-hamberger ' + list[1]}>
-                            </button>
-                        </div>
-                        <div className={'p-navbar__content ' + list[2]}>
-                            <ul></ul>
-                            <ul className='p-navbar__menu'>
-                                <li className='p-navbar__item' onClick={goContent2}>
-                                    <a href='#'>회사소개</a>
-                                </li>
-                                <li className='p-navbar__item' onClick={goContent3}>
-                                    <a href='#'>시술부위</a>
-                                </li>
-                                <li className='p-navbar__item' onClick={goContent4}>
-                                    <a href='#'>문의하기</a>
-                                </li>
+            <header className={`header  + ${clickHamburger ? "header-click" : "header-unclick"}`}>
+                <div className='header__con'>
+
+                    {/* 로고 */}
+                    <ul className='header__logo-con'>
+                        {/* <li className='header__logo'><a href='/'>H ROAD</a></li> */}
+                        <li className='header__hamburger'><a><img src={clickHamburger ? `/img/btn_x.png` : `/img/btn_hamburger.png`} style={{width: '24px'}} onClick={showList}/></a></li>
+                    </ul>
+                    
+                    {/* 메뉴 */}
+                    <ul className='header__category-con'>
+                        <li>
+                            <a onClick={() => showSubMenu(1)}>COMPANY</a>
+                            <ul className={`header__category-sub-con + ${clickMenu == 1 ? "block" : "" }`}>
+                                <li><a href='/company/introduction'>회사소개</a></li>
+                                <li><a href='/company/ourservice'>서비스안내</a></li>
                             </ul>
-                        </div>
-                    </div>
+                        </li>
+                        <li>
+                            <a onClick={() => showSubMenu(2)}>SERVICE</a>
+                            <ul className={`header__category-sub-con + ${clickMenu == 2 ? "block" : "" }`}>
+                                <li><a href='/service/weddingsong'>감동축가</a></li>
+                                <li><a href='/service/weddingmc'>센스사회</a></li>
+                                <li><a href='/service/weddingcamera'>촬영및편집</a></li>
+                                <li><a href='/service/weddingdirecting'>컨셉디렉팅</a></li>
+                                <li><a href='/service/weddingevent'>이벤트</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a onClick={() => showSubMenu(3)}>PORTFOLIO</a>
+                            <ul className={`header__category-sub-con + ${clickMenu == 3 ? "block" : "" }`}>
+                                <li><a href='/portfolio/portfolio'>포트폴리오</a></li>
+                                <li><a href='https://www.youtube.com/channel/UC2Edi1ZsZ_ywd30Tu3vE2zQ'>유튜브</a></li>
+                                <li><a href='https://www.instagram.com/sunpy.wedding'>인스타그램</a></li>
+                                <li><a href='https://blog.naver.com/sunpys_lifedance'>블로그</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a onClick={() => showSubMenu(4)}>REVIEW</a>
+                            <ul className={`header__category-sub-con + ${clickMenu == 4 ? "block" : "" }`}>
+                                <li><a href='/review/reason'>감동의이유</a></li>
+                                <li><a href='/review/review'>후기모음</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a onClick={() => showSubMenu(5)}>CONTACT</a>
+                            <ul className={`header__category-sub-con + ${clickMenu == 5 ? "block" : "" }`}>
+                                <li><a href='/contact/contact'>견적받기</a></li>
+                                <li><a href='https://open.kakao.com/o/sEZYh5Hc'>카톡상담</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                    {/* sns채널 */}
+                    <ul className='header__icon-con'>
+                        <li><a href='https://www.youtube.com/channel/UC2Edi1ZsZ_ywd30Tu3vE2zQ'>YOUTUBE</a></li>
+                        <li><a href='https://blog.naver.com/sunpys_lifedance'>BLOG</a></li>
+                        <li><a href='https://www.instagram.com/sunpy.wedding'>INSTA</a></li>
+                    </ul>
+
                 </div>
-            </nav>
+            </header>
         </>
     )
 }
