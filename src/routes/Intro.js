faa9faea0482cb67b230c595f301b987ce9baecf
quokkawoo <quokkawoo@gmail.com>
2023-11-14 14:42:07 +0900

    해결중

--- src/routes/Intro.js
+++ src/routes/Intro.js
@@ -172,16 +172,9 @@ export default function Intro(){
                     <div className={`fixed-bg-wrap slide-in00  + ${useScroll('.fixed-bg-wrap', 0.85).isShow ? 'slide-in01' : ''}`}>
                             <div className={`fixed-bg`}
                             style={{backgroundImage: 'url(/img/it05.jpg)',
-                            transform: `translateY(${150 + (useScroll('.fixed-bg-wrap', 0).stateY * 0.5) + 'px'})` 
+                            transform: `translateY(${(-200 + (useScroll('.fixed-bg-wrap', 0).stateY * 0.1)) + 'px'})` 
                             }}></div>
-                            {console.log(useScroll('.fixed-bg-wrap', 0).stateY )}
                     </div>
-                </div>
-                <div className='intro-brand__img-wrap'>
-                        <div className={`intro-brand__img slide-in00  + ${useScroll('.intro-brand__img-wrap', 0.85).isShow ? 'slide-in01' : ''}`}
-                         style={{backgroundImage: 'url(/img/it05.jpg)',
-                         transform: `translateY(${150 + (useScroll('.intro-brand__img-wrap', 0).stateY * 0.3) + 'px'})` 
-                         }}></div>
                 </div>
                 {/* <div className='intro-brand__img-wrap'>
                         <div className={`intro-brand__img slide-in00  + ${useScroll('.intro-brand__img-wrap', 0.85).isShow ? 'slide-in01' : ''}`}
