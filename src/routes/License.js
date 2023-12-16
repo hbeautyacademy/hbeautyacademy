13faa9d52e64e720832ea10b3fb7e54f291d5a70
hbeautyacademy <hbeautyacademy8@gmail.com>
2023-12-16 19:30:10 +0900

    start

--- src/routes/License.js
+++ src/routes/License.js
@@ -26,7 +26,7 @@ export default function License(){
     return(
         <>
             <div className='detail-page'>
-                <div className='detail-page__banner-con' style={{background: 'url(./img/test16.jpg)'}}>
+                <div className='detail-page__banner-con' style={{background: 'url(./img/license.jpg)'}}>
                     <div>LICENSE</div>
                 </div>
 
